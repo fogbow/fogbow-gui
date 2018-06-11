0954b471fc6f20fa116fc07450d55afd142a6a0f
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-06-11 17:17:49 -0300

    Add aggregate table.

--- openstack_dashboard/dashboards/fogbow/members/views.py
+++ openstack_dashboard/dashboards/fogbow/members/views.py
@@ -19,6 +19,8 @@ MEMBER_TERM = fogbow_models.FogbowConstants.MEMBER_TERM
 QUOTA_TERM = fogbow_models.FogbowConstants.QUOTA_TERM
 MAX_VALUE = 2000000000
 
+fowbow_endpoint = 'http://localhost:8080'
+
 class IndexView(tables.DataTableView):
     table_class = project_tables.MembersTable
     template_name = 'fogbow/members/index.html'
@@ -76,22 +78,34 @@ class IndexView(tables.DataTableView):
         
         return members
 
+
+
 def get_shared_quota(request, member_id):
-    response = requests.get('http://localhost:8080/quota/shared')
+    response = requests.get(fowbow_endpoint + '/quota/shared')
     r = response.text.encode('ascii')
     return HttpResponse(r)
 
 def get_available_quota(request, member_id):
-    response = requests.get('http://localhost:8080/quota/available')
+    response = requests.get(fowbow_endpoint + '/quota/available')
     r = response.text.encode('ascii')
     return HttpResponse(r)
 
 def get_used_by_me_quota(request, member_id):
-    response = requests.get('http://localhost:8080/quota/me')
+    response = requests.get(fowbow_endpoint + '/quota/me')
     r = response.text.encode('ascii')
     return HttpResponse(r)
 
 def getSpecificMemberQuota(request, member_id):
-    response = requests.get('http://localhost:8080/quota/member/2')
+    response = requests.get(fowbow_endpoint + '/quota/member/2')
+    r = response.text.encode('ascii')
+    return HttpResponse(r)
+
+def get_members(request):
+    response = requests.get(fowbow_endpoint + '/members')
+    r = response.text.encode('ascii')
+    return HttpResponse(r)
+
+def get_aggregated(request):
+    response = requests.get(fowbow_endpoint + '/quota/aggregated')
     r = response.text.encode('ascii')
     return HttpResponse(r)
