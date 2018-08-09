0e189d604718ccc26ccf0de53fa90f3dcdf34c77
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-09 15:08:05 -0300

    Create members page .

---
+++ src/pages/MembersPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MembersPage extends Component {
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MembersPage;
