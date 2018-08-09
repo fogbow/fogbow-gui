cd1d969731f364f8569362353c1f52ee4ac3a040
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-09 15:08:05 -0300

    Create members page .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import LoginPage from './pages/LoginPage';
+import MembersPage from './pages/MembersPage';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <main>
               <Switch>
                 <Route exact path="/" component={LoginPage} />
+                <Route exact path="/fogbow/members" component={MembersPage} />
               </Switch>
           </main>
         </BrowserRouter>
