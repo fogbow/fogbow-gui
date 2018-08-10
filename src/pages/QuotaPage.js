eeab71982b6f8e3cef64664bfe3d17c04ffff74a
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-08-10 11:32:50 -0300

    Change arquitecture to provide more easy re-use of sidebar.

---
+++ src/pages/QuotaPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class QuotaPage extends Component {
+    
+    render() {
+        return (
+            <p>opa quota</p>
+        );
+    }
+}
+
+export default QuotaPage;
