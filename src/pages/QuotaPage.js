faef7fce88fa09cccac1750d2bfa2557518c0536
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-08-11 18:27:10 -0300

    Fix on-chenge event when select a vendor in quota table.

--- src/pages/QuotaPage.js
+++ src/pages/QuotaPage.js
@@ -2,35 +2,47 @@ import React, { Component } from 'react';
 
 import QuotaTable from '../components/QuotaTable';
 
-class QuotaPage extends Component {
+const mockData = { 
+    totalQuota: { 
+        vCPU: 28,
+        ram: 73728,
+        instances: 11
+    },
+    usedQuota: { 
+        vCPU: 0,
+        ram: 0,
+        instances: 0
+    },
+    availableQuota: { 
+        vCPU: 28,
+        ram: 73728,
+        instances: 11
+    } 
+};
 
-    vendorchange = (event) => {
-        console.log(event.target.value);
+class QuotaPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quota: mockData,
+            selectedUserQuota: mockData
+        };
+    }
+    
+
+    vendorChange = (event) => {
+        event.preventDefault();
+        // TODO: Here we will get data.
     };
 
     render() {
-        const mockData = { 
-            totalQuota: { 
-                vCPU: 28,
-                ram: 73728,
-                instances: 11
-            },
-            usedQuota: { 
-                vCPU: 0,
-                ram: 0,
-                instances: 0
-            },
-            availableQuota: { 
-                vCPU: 28,
-                ram: 73728,
-                instances: 11
-            } 
-        };
         return (
             <div>
-                <QuotaTable label="Local" data={mockData}/>
-                <QuotaTable label="Aggregated" data={mockData}/>
-                <QuotaTable vendors={['naf1', 'naf2']} vendorchange={this.vendorchange} data={mockData}/>
+                <QuotaTable label="Local" data={this.state.quota}/>
+                <QuotaTable label="Aggregated" data={this.state.quota}/>
+                <QuotaTable vendors={['naf1', 'naf2']} 
+                    vendorChange={this.vendorChange} 
+                    data={this.state.selectedUserQuota}/>
             </div>
         );
     }
