cf0fc739010f7dc9eb9c05951d1eefd39259135f
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-14 10:24:25 -0300

    Connect Computes page to reducer and make request to get computes.

--- src/actions/computes.actions.js
+++ src/actions/computes.actions.js
@@ -4,9 +4,9 @@ import ComputesProvider from '../providers/computes.provider';
 export const getComputes = () => {
     return dispatch => {
         return new Promise((resolve, reject) => {
-            const request = () => ({ type: computesActionsTypes.GET_MEMBERS_REQUEST});
-            const success = (computes) => ({ type: computesActionsTypes.GET_MEMBERS_SUCCESS, computes });
-            const failure = (error) => ({ type: computesActionsTypes.GET_MEMBERS_FAILURE, error });
+            const request = () => ({ type: computesActionsTypes.GET_COMPUTES_REQUEST});
+            const success = (computes) => ({ type: computesActionsTypes.GET_COMPUTES_SUCCESS, computes });
+            const failure = (error) => ({ type: computesActionsTypes.GET_COMPUTES_FAILURE, error });
 
             dispatch(request());
 
