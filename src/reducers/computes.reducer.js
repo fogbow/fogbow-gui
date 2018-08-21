db18ecab12f557823e43468692667a6ba442db0b
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-21 15:26:32 -0300

    Add request to get computes status.

--- src/reducers/computes.reducer.js
+++ src/reducers/computes.reducer.js
@@ -2,14 +2,14 @@ import { computesActionsTypes } from '../actions/computes.actions.types';
 
 const computes = (state = {loading: false}, action) => {
     switch (action.type) {
-        case computesActionsTypes.GET_MEMBERS_REQUEST:
+        case computesActionsTypes.GET_COMPUTES_REQUEST:
             return { loading: false };
-        case computesActionsTypes.GET_MEMBERS_SUCCESS:
+        case computesActionsTypes.GET_COMPUTES_SUCCESS:
             return {
                 data: action.computes,
                 loading: true
             };
-        case computesActionsTypes.GET_MEMBERS_FAILURE:
+        case computesActionsTypes.GET_COMPUTES_FAILURE:
             return { error: action.error };
         
         default:
