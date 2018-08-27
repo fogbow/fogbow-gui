2c91e7935d0144e59def80c1cd1cf4faf6760930
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-27 15:32:57 -0300

    Add base form to create a compute order.

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import OrderComponent from './OrderComponent';
+import ComputeForm from './ComputeForm';
 
 const headers = [
     'id', 'Provider', 'State', 'Actions'
@@ -64,25 +65,7 @@ class OrderList extends Component {
 
                             <button type="button" className="btn btn-btn-dark my-2 my-sm-0" data-toggle="modal" data-target="#form">Create</button>
 
-                            <div className="modal fade" id="form" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog" role="document">
-                                <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div className="modal-body">
-                                    ...
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" className="btn btn-primary">Save changes</button>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
+                            <ComputeForm/>
                         </form>
                     </div>
                 </nav>
