913db0df329e90d1716277bc8eec528e5505c902
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-08-29 13:01:05 -0300

    Add fednetworks form.

---
+++ src/components/FederatedNetworksForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class FederatedNetworksForm extends Component {
+    render() {
+        return (
+            <div className="modal fade" id="form" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Create Network</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <label>Label</label>
+                        <input className="form-control" type="text"/>
+                        
+                        <label>CIDR</label>
+                        <input className="form-control" type="text"/>
+
+                        <label>Members</label>
+                        <select name='member' className="form-control">
+                            <option value=''></option>
+                            {/* { this.props.members.data.map((member, idx) => <option key={idx} value={member}>{member}</option>) } */}
+                        </select>
+
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Create federated network</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FederatedNetworksForm;
