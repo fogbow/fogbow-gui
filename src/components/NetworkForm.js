6bee1f78d1808f75fdab4ac557f467c5da5af818
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-09-03 13:35:13 -0300

    Add state and default values to create network form.

--- src/components/NetworkForm.js
+++ src/components/NetworkForm.js
@@ -5,9 +5,9 @@ import _ from 'lodash';
 import { createNetwork } from '../actions/networks.actions';
 
 const initialState = {
-    address: '',
-    gateway: '',
-    allocation: '',
+    address: '10.10.0.0/24',
+    gateway: '10.10.0.1',
+    allocation: 'dynamic',
     member: ''
 }
 
@@ -46,19 +46,21 @@ class NetworkForm extends Component {
                     </div>
                     <div className="modal-body">
                         <label>CIDR</label>
-                        <input className="form-control" type="text" name="address" onChange={this.handleChange}/>
+                        <input className="form-control" type="text" name="address" 
+                           value={this.state.address} onChange={this.handleChange}/>
 
                         <label>Gateway</label>
-                        <input className="form-control" type="text" name="gateway" onChange={this.handleChange}/>
+                        <input className="form-control" type="text" name="gateway" 
+                           value={this.state.gateway} onChange={this.handleChange}/>
 
                         <label>Allocation</label>
-                        <select  name='allocation' className="form-control" onChange={this.handleChange}>
-                            <option value='dynamic'>dynamic</option>
-                            <option value='static'>static</option>
+                        <select  name='allocation' className="form-control" value={this.state.allocation} onChange={this.handleChange}>
+                            <option value='dynamic'>Dynamic</option>
+                            <option value='static'>Static</option>
                         </select>
 
                         <label>Member</label>
-                        <select name='member' className="form-control" onChange={this.handleChange}>
+                        <select name='member' className="form-control" value={this.state.member} onChange={this.handleChange}>
                             <option value=''></option>
                             { this.props.members.data.map((member, idx) => <option key={idx} value={member}>{member}</option>) }
                         </select>
@@ -66,7 +68,7 @@ class NetworkForm extends Component {
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Create Compute</button>
+                        <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Create Network</button>
                     </div>
                     </div>
                 </div>
