b185e60513a1a7624118fdc241d4ccaa1e0bfaba
Paulo Ewerton <pauloewerton@lsd.ufcg.edu.br>
2018-10-05 12:05:41 -0300

    Renaming member att to providingMember to comply with api

--- src/components/NetworkForm.js
+++ src/components/NetworkForm.js
@@ -5,86 +5,92 @@ import _ from 'lodash';
 import { createNetwork } from '../actions/networks.actions';
 
 const initialState = {
-    name: '',
-    address: '10.10.0.0/24',
-    gateway: '10.10.0.1',
-    allocation: 'dynamic',
-    member: ''
+  name: '',
+  address: '10.10.0.0/24',
+  gateway: '10.10.0.1',
+  allocation: 'dynamic',
+  providingMember: ''
 }
 
 class NetworkForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
-    }
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
 
-    handleChange = (event) => {
-        let { name, value } = event.target;
-        
-        this.setState({
-            [name]: value
-        });
-    };
+  handleChange = (event) => {
+    let { name, value } = event.target;
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        let body = _.pickBy(this.state, _.identity);
-        
-        let { dispatch } = this.props;
-        dispatch(createNetwork(body));
-        this.resetForm();
-    };
+    this.setState({
+        [name]: value
+    });
+  };
 
-    resetForm = () => {
-        this.setState(initialState);
-    };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let body = _.pickBy(this.state, _.identity);
 
-    render() {
-        return (
-            <div className="modal fade" id="form" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Create Network</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <label>Name</label>
-                        <input value={this.state.name} onChange={this.handleChange}
-                        className="form-control" type="text" name="name"/> 
+    let { dispatch } = this.props;
+    dispatch(createNetwork(body));
+    this.resetForm();
+  };
 
-                        <label>CIDR</label>
-                        <input className="form-control" type="text" name="address" 
-                           value={this.state.address} onChange={this.handleChange}/>
+  resetForm = () => {
+      this.setState(initialState);
+  };
 
-                        <label>Gateway</label>
-                        <input className="form-control" type="text" name="gateway" 
-                           value={this.state.gateway} onChange={this.handleChange}/>
-
-                        <label>Allocation</label>
-                        <select  name='allocation' className="form-control" value={this.state.allocation} onChange={this.handleChange}>
-                            <option value='dynamic'>Dynamic</option>
-                            <option value='static'>Static</option>
-                        </select>
-
-                        <label>Member</label>
-                        <select name='member' className="form-control" value={this.state.member} onChange={this.handleChange}>
-                            <option value=''></option>
-                            { this.props.members.data.map((member, idx) => <option key={idx} value={member}>{member}</option>) }
-                        </select>
-
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handleSubmit}>Create Network</button>
-                    </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="modal fade" id="form" tabIndex="-1" role="dialog"
+           aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Create Network</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-        );
-    }
+            <div className="modal-body">
+              <label>Name</label>
+              <input value={this.state.name} onChange={this.handleChange} className="form-control"
+                     type="text" name="name"/>
+
+              <label>CIDR</label>
+              <input className="form-control" type="text" name="address" value={this.state.address}
+                     onChange={this.handleChange}/>
+
+              <label>Gateway</label>
+              <input className="form-control" type="text" name="gateway" value={this.state.gateway}
+                     onChange={this.handleChange}/>
+
+              <label>Allocation</label>
+              <select name='allocation' className="form-control" value={this.state.allocation}
+                      onChange={this.handleChange}>
+                  <option value='dynamic'>Dynamic</option>
+                  <option value='static'>Static</option>
+              </select>
+
+              <label>Providing Member</label>
+              <select name='providingMember' className="form-control" value={this.state.providingMember}
+                      onChange={this.handleChange}>
+                  <option value=''></option>
+                  {
+                    this.props.members.data.map((member, idx) =>
+                      <option key={idx} value={member}>{member}</option>)
+                  }
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary" data-dismiss="modal"
+                      onClick={this.handleSubmit}>Create Network</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const stateToProps = state => ({
--- src/components/PublicIpForm.js
+++ src/components/PublicIpForm.js
@@ -8,90 +8,98 @@ import { getComputes } from '../actions/computes.actions';
 import { createPublicIp } from '../actions/publicIps.actions';
 
 const initialState = {
-    providingMember: '',
-    computeOrderId: '',
+  providingMember: '',
+  computeOrderId: '',
 };
 
 class PublicIpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  componentDidMount = () => {
+    let { dispatch } = this.props;
+    if(! this.props.computes.loading) {
+        dispatch(getComputes());
     }
+  };
 
-    componentDidMount = () => {
-        let { dispatch } = this.props;
-        if(! this.props.computes.loading) {
-            dispatch(getComputes());
-        }
-    };
+  handleChange = (event) => {
+    let { name, value } = event.target;
 
-    handleChange = (event) => {
-        let { name, value } = event.target;
+    this.setState({
+        [name]: value
+    });
+  };
 
-        this.setState({
-            [name]: value
-        });
-    };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let body = _.pickBy(this.state, _.identity);
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        let body = _.pickBy(this.state, _.identity);
+    let { dispatch } = this.props;
+    dispatch(createPublicIp(body));
+    this.resetForm();
+  };
 
-        let { dispatch } = this.props;
-        dispatch(createPublicIp(body));
-        this.resetForm();
-    };
+  resetForm = () => {
+    this.setState(initialState);
+  };
 
-    resetForm = () => {
-        this.setState(initialState);
-    };
-
-    render() {
-        return (
-            <div className="modal fade" id="form" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Create Public IP</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <label>Compute</label>
-                            <select  name='computeOrderId' className="form-control" required
-                                value={this.state.source} onChange={this.handleChange}>
-                                <option value=''></option>
-                                {
-                                    this.props.computes.loading ?
-                                    this.props.computes.data
-                                        .filter(volume => volume.state === 'READY')
-                                        .map((compute, idx) => <option key={idx} value={compute.instanceId}>{compute.instanceId}</option>):
-                                    undefined
-                                }
-                            </select>
-
-                            <label>Member</label>
-                            <select name='providingMember' className="form-control" required 
-                                value={this.state.member} onChange={this.handleChange}>
-                                <option value=''></option>
-                                { this.props.members.data.map((member, idx) => <option key={idx} value={member}>{member}</option>) }
-                            </select>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary"  data-dismiss="modal" onClick={this.handleSubmit}>Create Public IP</button>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="modal fade" id="form" tabIndex="-1" role="dialog"
+           aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Create Public IP</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-        );
-    }
+            <div className="modal-body">
+              <label>Compute</label>
+              <select  name='computeOrderId' className="form-control" required
+                  value={this.state.source} onChange={this.handleChange}>
+                  <option value=''></option>
+                  {
+                    this.props.computes.loading ?
+                    this.props.computes.data
+                        .filter(volume => volume.state === 'READY')
+                        .map((compute, idx) =>
+                          <option key={idx} value={compute.instanceId}>{compute.instanceId}</option>):
+                    undefined
+                  }
+              </select>
+
+              <label>Member</label>
+              <select name='providingMember' className="form-control" required
+                      value={this.state.providingMember} onChange={this.handleChange}>
+                <option value=''></option>
+                {
+                  this.props.members.data.map((member, idx) =>
+                    <option key={idx} value={member}>{member}</option>)
+                }
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary"  data-dismiss="modal"
+                      onClick={this.handleSubmit}>
+                Create Public IP
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const stateToProps = state => ({
-    members: state.members,
-    computes: state.computes
+  members: state.members,
+  computes: state.computes
 });
 
 export default connect(stateToProps)(PublicIpForm);
