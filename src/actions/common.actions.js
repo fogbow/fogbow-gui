e7730d2813b6ca5d778bf537b860551cc87ab010
Jadson Luan <jadsonluan1@gmail.com>
2020-02-13 16:34:23 -0300

    Fix indentation

--- src/actions/common.actions.js
+++ src/actions/common.actions.js
@@ -1,38 +1,38 @@
 export const getAllocations = (providerId, cloudNames, dispatch, resourceProvider, actionTypes) => {
-    return new Promise((resolve, reject) => {
-        const { request, success, failure } = actionTypes;
+  return new Promise((resolve, reject) => {
+    const { request, success, failure } = actionTypes;
 
-        dispatch(request());
+    dispatch(request());
 
+    try {
+      let promises = cloudNames.map(async (cloudName) => {
         try {
-            let promises = cloudNames.map(async(cloudName) => {
-                try {
-                    let response = await resourceProvider.getAllocation(providerId, cloudName);
-                    let allocation = response.data;
-                    return {
-                        ... allocation,
-                        cloudName
-                    };
-                } catch (error) {
-                    console.log(error);
-                    throw error;
-                }
-            });
-
-            Promise.all(promises).then(result => {
-                const allocations = {};
-                result.forEach(allocation => {
-                    allocations[allocation.cloudName] = {};
-                    Object.assign(allocations[allocation.cloudName], allocation);
-                    delete allocations[allocation.cloudName].cloudName;
-                })
-
-                resolve(dispatch(success(allocations)));
-            })
+          let response = await resourceProvider.getAllocation(providerId, cloudName);
+          let allocation = response.data;
+          return {
+            ...allocation,
+            cloudName
+          };
         } catch (error) {
-            // const message = getErrorMessage(error);
-            // toast.error(messages.images.get.concat(message));
-            reject(dispatch(failure(error)));
+          console.log(error);
+          throw error;
         }
-    });
+      });
+
+      Promise.all(promises).then(result => {
+        const allocations = {};
+        result.forEach(allocation => {
+          allocations[allocation.cloudName] = {};
+          Object.assign(allocations[allocation.cloudName], allocation);
+          delete allocations[allocation.cloudName].cloudName;
+        })
+
+        resolve(dispatch(success(allocations)));
+      })
+    } catch (error) {
+      // const message = getErrorMessage(error);
+      // toast.error(messages.images.get.concat(message));
+      reject(dispatch(failure(error)));
+    }
+  });
 };
