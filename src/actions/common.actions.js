fb8395bbfdde4b53eef22c0dd5ed82e027f2beb4
Jadson Luan <jadsonluan1@gmail.com>
2020-02-13 16:42:44 -0300

    Move getAllocation method

--- src/actions/common.actions.js
+++ src/actions/common.actions.js
@@ -1,3 +1,42 @@
+/**
+ * Get user allocation from a specified cloud and resource.
+ * 
+ * Example of actionTypes parameter:
+ *  
+ * const request = () => ({ type: volumesActionsTypes.GET_VOLUME_ALLOCATION_REQUEST});
+ * const success = (allocation) => ({ type: volumesActionTypes.GET_VOLUME_ALLOCATION_SUCCESS, allocation });
+ * const failure = (error) => ({ type: volumesActionTypes.GET_VOLUME_ALLOCATION_FAILURE, error });
+ * const actionTypes = { request, success, failure };
+ * 
+ * @param {String} providerId 
+ * @param {String} cloudName 
+ * @param {*} dispatch
+ * @param {Provider} resourceProvider a instance of Provider (Example: VolumesProvider, ComputesProvider, etc)
+ * @param {Object} request object containing three functions (action types): request, success and failure. 
+ */
+export const getAllocation = (providerId, cloudName, dispatch, resourceProvider, actionTypes) => {
+  return new Promise(async (resolve, reject) => {
+    const { request, success, failure } = actionTypes;
+
+    dispatch(request());
+
+    try {
+      try {
+        let allocation = await resourceProvider.getAllocation(providerId, cloudName);
+        resolve(dispatch(success(allocation.data)));
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    } catch (error) {
+      // TODO(jadsonluan): Add new messages to this request
+      // const message = getErrorMessage(error);
+      // toast.error(messages.allocations.get.concat(message));
+      reject(dispatch(failure(error)));
+    }
+  });
+};
+
 export const getAllocations = (providerId, cloudNames, dispatch, resourceProvider, actionTypes) => {
   return new Promise((resolve, reject) => {
     const { request, success, failure } = actionTypes;
@@ -30,6 +69,7 @@ export const getAllocations = (providerId, cloudNames, dispatch, resourceProvide
         resolve(dispatch(success(allocations)));
       })
     } catch (error) {
+      // TODO(jadsonluan): Add new messages to this request
       // const message = getErrorMessage(error);
       // toast.error(messages.images.get.concat(message));
       reject(dispatch(failure(error)));
