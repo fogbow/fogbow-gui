5f6db10ae9f0ecc66ddef44cb0ddb38f22926b5f
Jadson Luan <jadson.silva@ccc.ufcg.edu.br>
2020-03-04 15:51:44 -0300

    Fix memory quota displaying negative values

--- src/defaults/api.config.js
+++ src/defaults/api.config.js
@@ -2,13 +2,13 @@ export const env = {
   serverEndpoint: '',
   deployType: 'basic-site',
   fns: 'http://localhost:8083/fns',
-  ras: 'http://localhost:8082/ras',
+  ras: 'http://localhost:8080/ras',
   ms: 'http://localhost:8084/ms',
   as: 'http://localhost:8081/as',
-  local: 'atm-test-site1.lsd.ufcg.edu.br',
+  local: 'jlss.lsd.ufcg.edu.br',
   remoteCredentialsUrl: '',
   refreshTime: 5000,
-  authenticationPlugin: 'LDAP',
+  authenticationPlugin: 'KeystoneV3',
   credentialFields: {
     username: {
       type: 'text',
