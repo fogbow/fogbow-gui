011507de2c86e1729bb5a79583d2c5daa5008543
Jadson Luan <jadsonluan1@gmail.com>
2020-11-16 12:10:08 -0300

    Refactor actions dropdown menu

---
+++ src/components/OrderActions.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class OrderActions extends Component {
+  render() {
+    return (
+      <div className='btn-group' role='group'>
+        <button className='btn btn-secondary dropdown-toggle'
+          data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'
+          id='btnGroupDrop1'>
+        </button>
+        <div className='dropdown-menu order-dropdown' aria-labelledby='btnGroupDrop1'>
+          { this.props.actions.map(action => (
+            <button 
+              className='dropdown-item' 
+              onClick={action.onClick}
+              value={action.value} data-toggle='modal'
+              data-target={action.modalId}>
+            {action.text}
+            </button>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect()(OrderActions);
